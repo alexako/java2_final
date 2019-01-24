82b9ea28fea96a5f48bfa1c05dfc67b0497f0e98
Alex Reyes <me@alexjreyes.com>
2019-01-24 15:23:13 +0800

    Add menu bar

--- src/taskmanagement/GUI.java
+++ src/taskmanagement/GUI.java
@@ -51,13 +51,35 @@ public class GUI extends Application {
         column1.setSpacing(5);
         column2.setSpacing(5);
         column3.setSpacing(5);
-        column1.setPrefWidth(250);
-        column2.setPrefWidth(250);
-        column3.setPrefWidth(250);
+        column1.setPrefWidth(240);
+        column2.setPrefWidth(240);
+        column3.setPrefWidth(240);
 
         gridPane.add(column1, 0, 0);
         gridPane.add(column2, 1, 0);
         gridPane.add(column3, 2, 0);
+
+        // create a menu 
+        Menu m = new Menu("File"); 
+
+        // create menuitems 
+        MenuItem m1 = new MenuItem("menu item 1"); 
+        MenuItem m2 = new MenuItem("menu item 2"); 
+        MenuItem m3 = new MenuItem("menu item 3"); 
+
+        // add menu items to menu 
+        m.getItems().add(m1); 
+        m.getItems().add(m2); 
+        m.getItems().add(m3); 
+
+        // create a menubar 
+        MenuBar mb = new MenuBar(); 
+
+        // add menu to menubar 
+        mb.getMenus().add(m); 
+
+        // create a VBox 
+        VBox vb = new VBox(mb); 
 
         FileHandler f = new FileHandler();
         f.loadUsers();
@@ -120,49 +142,50 @@ public class GUI extends Application {
         }
         for (int i = 0; i < cards.size() - 5; i++) {
             Button cardContainer = new Button(cards.get(i).getTitle());
-            cardContainer.setStyle("-fx-background-color: white;");
+            cardContainer.setStyle("-fx-background-color: white; -fx-pref-width: 200px;");
             cardContainer.setAlignment(Pos.BASELINE_LEFT);
             cardContainer.setEffect(ds);
             cardContainer.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_CLICKED, eventHandler);
             column2.getChildren().add(cardContainer);
         }
         Button cardContainer = new Button(cards.get(7).getTitle());
-        cardContainer.setStyle("-fx-background-color: white;");
+        cardContainer.setStyle("-fx-background-color: white; -fx-pref-width: 200px;");
         cardContainer.setAlignment(Pos.BASELINE_LEFT);
         cardContainer.setEffect(ds);
         cardContainer.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_CLICKED, eventHandler);
         column3.getChildren().add(cardContainer);
 
-        Label connectionStatus = new Label("Not connected ");
-        Circle c = new Circle(8, Color.RED);
-        Group circle = new Group(c);
-
-        HBox connectionIndicator = new HBox();
-        connectionIndicator.getChildren().addAll(connectionStatus, circle);
-
-        Region region = new Region();
-        HBox.setHgrow(region, Priority.ALWAYS);
+        Label projectName = new Label("Project Title");
 
         HBox header = new HBox();
-        header.getChildren().addAll(region, connectionIndicator);
+        header.getChildren().addAll(projectName);
+        header.setAlignment(Pos.CENTER);
 
         ComboBox userSelection = new ComboBox();
         userSelection.getItems().addAll(users);
 
-        HBox hBox = new HBox();
-        hBox.setPadding(new Insets(10,10,10,10));
-        hBox.setSpacing(10);
-        hBox.getChildren().addAll(titleInput, descriptionInput, userSelection, addButton);
+        HBox control = new HBox();
+        control.setPadding(new Insets(10,10,10,10));
+        control.setSpacing(10);
+        control.getChildren().addAll(titleInput, descriptionInput, userSelection, addButton);
 
         ScrollPane scrollPane = new ScrollPane();
         scrollPane.setContent(gridPane);
-        scrollPane.setStyle("-fx-stroke-width: 0");
+        scrollPane.setPrefHeight(500);
 
-        BorderPane root = new BorderPane();
-        root.setPadding(new Insets(20));
-        root.setTop(header);
-        root.setCenter(scrollPane);
-        root.setBottom(hBox);
+
+        VBox root = new VBox();
+        BorderPane content = new BorderPane();
+        content.setPadding(new Insets(20));
+        content.setTop(header);
+        content.setCenter(scrollPane);
+
+        // Gap
+        Region spacer = new Region();
+        root.setVgrow(spacer, Priority.ALWAYS);
+
+        root.getChildren().addAll(mb, content, spacer, control);
+
         Scene scene = new Scene(root, 800, 600);
         window.setScene(scene);
         window.show();
@@ -185,6 +208,7 @@ public class GUI extends Application {
         // private User assignedUser;
         // private int boardId;
         // private int columnIndex;
+
 
         Card card = new Card();
         card.setTitle(titleInput.getText());
